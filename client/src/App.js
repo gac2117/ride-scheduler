729d0bb0c142bc656ca36a6e2b9b5f3b9dbbf28e
Grace Choi <oiograceoio@gmail.com>
2020-02-04 20:13:16 -0500

    add redux

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavbar from './components/AppNavbar';
 import RideList from './components/RideList';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
-    <div className='App'>
-      <AppNavbar />
-      <RideList />
-    </div>
+    <Provider store={store}>
+      <div className='App'>
+        <AppNavbar />
+        <RideList />
+      </div>
+    </Provider>
   );
 }
 
